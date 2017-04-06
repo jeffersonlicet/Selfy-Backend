2c0476fc154c5acaad60d5fe67829dfcd7b6d9e7
Jeff <jeffersonlicet>
2017-04-06 15:00:39 -0400

    Updating android request

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -142,7 +142,7 @@ class AuthController extends Controller
 
                 return response()->json([
                     'status' => FALSE,
-                    'report' => 'user_not_found'
+                    'report' => 'User not found'
                 ]);
             }
 
