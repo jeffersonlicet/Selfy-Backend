1fb205d171c737eff1db2aeb9721fdbb6cd586f2
Jeff <jeffersonlicet>
2017-05-11 19:04:54 -0400

    #15 - Create user random suggestion getter

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,7 @@ Route::group(
 
         Route::get('likes', 'Api\LikeController@index');
         Route::post('user/avatar', 'Api\UserController@avatar');
-
+//
         Route::get('users/suggestions', 'Api\UserController@suggestions');
         Route::get('users/duo', 'Api\UserController@duo');
         Route::get('users/search', 'Api\UserController@search');
