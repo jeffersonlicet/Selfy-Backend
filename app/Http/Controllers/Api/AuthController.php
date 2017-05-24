baf87b3659cc68a09102857b035315ff180b1c60
Jeff <jeffersonlicet>
2017-05-23 20:42:24 -0400

    Facebook integration system

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -558,7 +558,6 @@ class AuthController extends Controller
                 }
             }
 
-
             return response()->json([
                 'status' => FALSE,
                 'report' => 'invalid_key'
