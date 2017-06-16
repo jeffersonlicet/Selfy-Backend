abe239e47dd894d65a12492ed9ef5fbc25aa9ad8
Jeff <jeffersonlicet>
2017-06-16 10:48:38 -0400

    PlayChallenges Activation

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -66,38 +66,49 @@
     </section>
     <section class="section-4" id="duo">
         <div class="container">
+            <div class="row visible-xs">
+                <div class="col-xs-12 device-container">
+                    <img  src="{{ URL::asset('/images/duo_s.png') }}" class="img-responsive" alt="home photo">
+                </div>
+            </div>
             <div class="row">
-                <div class="col-md-4 col-md-offset-1">
+                <div class="col-md-6 col-sm-6 col-md-offset-1 col-sm-offset-1">
                     <div class="header-content">
                         <div class="header-content-inner">
                             <h1>Duo</h1>
                             <h3>A Duo is a face-based challenge. <br /> Selfy uses face recognition technology to encourage people to complete Duo challenges that can be completed by taking photos with your family, friends or another Selfy user.</h3>
                             <a href="javascript:void(0)" class="btn btn-info btn-fab" id="continue2"><i class="material-icons">keyboard_arrow_down</i></a>
                         </div>
-
                     </div>
                 </div>
 
-                <div class="col-md-5 col-sm-4 device-container col-xs-4">
-                    <img  src="{{ URL::asset('/images/duo.png') }}" class="img-responsive" alt="home photo">
+                <div class="col-md-4 col-sm-4 device-container hidden-xs">
+                    <img  src="{{ URL::asset('/images/duo_s.png') }}" class="img-responsive" alt="home photo">
                 </div>
             </div>
         </div>
     </section>
     <section class="section-5" id="spot">
-        <div class="container-fluid">
+        <div class="container">
+            <div class="row visible-xs">
+                <div class="col-xs-12 device-container">
+                    <img  src="{{ URL::asset('/images/duo_s.png') }}" class="img-responsive" alt="home photo">
+                </div>
+            </div>
             <div class="row">
 
-                <div class="col-md-6 col-sm-4 bottom-device"></div>
 
-                <div class="col-md-4">
+                <div class="col-md-4 col-sm-4 device-container hidden-xs">
+                    <img  src="{{ URL::asset('/images/duo_s.png') }}" class="img-responsive" alt="home photo">
+                </div>
+
+                <div class="col-md-6 col-sm-6 col-md-offset-1 col-sm-offset-1">
                     <div class="header-content">
                         <div class="header-content-inner">
                             <h1>Spot</h1>
                             <h3>A Spot is a place-based challenge.<br /> To complete a Spot challenge you must visit the indicated place and take a picture there. We will use your location to detect if you meet the challenge.</h3>
-                            <a href="javascript:void(0)" class="btn btn-default btn-fab" id="continue2"><i class="material-icons">keyboard_arrow_down</i></a>
+                            <a href="javascript:void(0)" class="btn btn-info btn-fab" id="continue2"><i class="material-icons">keyboard_arrow_down</i></a>
                         </div>
-
                     </div>
                 </div>
             </div>
