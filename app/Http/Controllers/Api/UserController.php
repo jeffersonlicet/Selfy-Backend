84911372446ef61a5b31aba2ee7eb4b58401bd37
Jeff <jeffersonlicet>
2017-07-11 15:21:54 -0400

    Removed random from suggestions

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1148,7 +1148,7 @@ class UserController extends Controller
             $following  = \Auth::user()->Following->pluck('following_id');
             $following[] = \Auth::user()->user_id;
 
-            $suggestion =  User::whereNotIn('user_id', $following)->take($limit)->offset($limit*$page)->orderBy('followers_count',  'ASC')->get();
+            $suggestion =  User::whereNotIn('user_id', $following)->orderBy('followers_count',  'ASC')->limit($limit)->offset($limit*$page)->get();
 
             return response()->json([
                 'status' => TRUE,
