4ea943fafd8a434faebafde7c4e6454287aef54a
Jeff <jeffersonlicet@gmail.com>
2017-09-17 13:18:01 -0400

    Override reset method to reset password type

--- app/Http/Controllers/App/ForgotPasswordController.php
+++ app/Http/Controllers/App/ForgotPasswordController.php
@@ -30,26 +30,30 @@ class ForgotPasswordController extends Controller
             'pageTitle'=> __('app.selfy_support'), 'token' => $token, 'metaTags' => null])->render();
     }
 
-    /**
-     * Reset the given user's password.
-     *
-     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
-     * @param  string  $password
-     * @return void
-     */
-    protected function resetPassword($user, $password)
+    protected function reset(Request $request)
     {
-        $user->forceFill([
-            'password' => bcrypt($password),
-            'remember_token' => Str::random(60),
-        ])->save();
+        $this->validate($request, $this->rules(), $this->validationErrorMessages());
 
-        $this->guard()->login($user);
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
 
         if(!\Auth::guest())
         {
             \Auth::user()->password_type = config('constants.APP_PLATFORMS.android');
             \Auth::user()->save();
         }
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+            ? $this->sendResetResponse($response)
+            : $this->sendResetFailedResponse($request, $response);
     }
 }
