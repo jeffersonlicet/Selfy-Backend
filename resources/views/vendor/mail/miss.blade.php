237b3f3153b2dc9b87367b8ecc80b79723636e55
Jeff <jeffersonlicet@gmail.com>
2017-10-18 15:12:53 -0400

    Fix account creation & update

--- resources/views/vendor/mail/miss.blade.php
+++ resources/views/vendor/mail/miss.blade.php
@@ -1,22 +1,29 @@
 @component('mail::message')
+
+{{ HTML::image('img/view1_principal_r_interlaced.png', 'a picture') }}
+
 Hola / Hello / Olá! <br />
 
 Español:
-{{ $name }}, hemos renovado Selfy para que puedas seguir disfrutando y compartiendo fotos con tus amigos.
-El chat ha sido mejorado y hemos corregido varios errores. Con la nueva version para Android podrás conocer a más personas.
-Te esperamos de nuevo. Hemos reinventado los Selfy-retos, que estáran disponibles pronto para Windows Phone, ya puedes probarlos si dispones de un Android.
+{{ $name }}, hemos renovado Selfy para que puedas seguir disfrutando y compartiendo fotos con tus amigos. <br />
+El chat ha sido mejorado y hemos corregido varios errores. Con la nueva version para Android podrás conocer a más personas. <br />
+Te esperamos de nuevo. <br />PD: Hemos reinventado los Selfy-retos, que estáran disponibles pronto para Windows Phone, ya puedes probarlos si dispones de un Android.
 
 English:
-{{ $name }}, we have renewed Selfy so you can enjoy and share photos with your friends.
-The chat has been improved and we have fixed several errors. With the new version for Android you can meet more people.
-We are waiting for you again. We have reinvented the Selfy-challenges, which will be available soon for Windows Phone, you can try them if you have an Android.
+{{ $name }}, we have renewed Selfy so you can enjoy and share photos with your friends. <br />
+The chat has been improved and we have fixed several errors. With the new version for Android you can meet more people. <br />
+We are waiting for you again. <br /> We have reinvented the Selfy-challenges, which will be available soon for Windows Phone, you can try them if you have an Android.
 
 Portugués:
-{{ $name }}, nós renovamos o Selfy para que você possa desfrutar e compartilhar fotos com seus amigos.
-O bate-papo foi melhorado e corrigimos vários erros. Com a nova versão do Android, você pode conhecer mais pessoas.
-Nós estamos esperando por você novamente. Nós reinventamos os desafios Selfy, que estarão disponíveis em breve para o Windows Phone, você pode experimentá-los se você tiver um Android.
+{{ $name }}, nós renovamos o Selfy para que você possa desfrutar e compartilhar fotos com seus amigos.<br />
+O bate-papo foi melhorado e corrigimos vários erros. Com a nova versão do Android, você pode conhecer mais pessoas.<br />
+Nós estamos esperando por você novamente.<br /> Nós reinventamos os desafios Selfy, que estarão disponíveis em breve para o Windows Phone, você pode experimentá-los se você tiver um Android.
 
+<br />
+Website:
 http://getselfy.net
+<br />
 Twitter: http://twitter.com/getselfy
+<br />
 The Selfy team.
 @endcomponent
