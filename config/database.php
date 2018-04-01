843fa4f15cac0ecfccec6abbc5419fc69c2c6ec9
Jefferson Licet <jeffersonlicet@gmail.com>
2018-03-31 21:08:04 -0300

    fix redix

--- config/database.php
+++ config/database.php
@@ -115,12 +115,13 @@ return [
     'redis' => [
 
         'client' => 'predis',
-
+        'read_write_timeout' => -1,
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
+            'read_write_timeout' => -1
         ],
 
     ],
